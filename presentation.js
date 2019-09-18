d331539ebf3e541bfb803b1cef7bb0fc04bf1829
Eloi Turpin <eloi.turpin@gmail.com>
2019-09-18 16:49:56 +0200

    fin tp

--- presentation.js
+++ presentation.js
@@ -11,7 +11,6 @@ var rl = readline.createInterface({
 
 function start() {
 
-
     function recupLogin(idRl, mdpRl) {
 
         var identifiant = idRl;
@@ -35,13 +34,14 @@ function start() {
         } else {
             service.initPost(identifiant, mdp,
                 function (err, res) {
-                    console.log(res.statusCode);
+
                     if (res.statusCode != 200) {
 
-                        console.log('mauvais identifiant/mdp');
+                        console.log(res.statusCode, 'mauvais identifiant/mdp');
                         recupLogin(null, null);
                     } else {
-
+                        console.log(res.statusCode, ' authentification réussie');
+                        menu();
                     }
 
 
@@ -67,18 +67,138 @@ function resultatConnection(params) {
 
 function menu() {
     console.log("1. Rechercher un collègue");
+    console.log('2. Créer un collègue');
+    console.log("3. Modifier l'email");
+    console.log('4. Modifier la photo');
     console.log("99. Sortir");
 
     // récupération de la saisie utilisateur
-    rl.question('Votre choix : ', function (saisie) {
+    rl.question('> ', function (saisie) {
 
         switch (saisie) {
             case "1":
-                console.log('Recherche en cours du nom xxx');
-                menu();
+                console.log('saisissez un nom')
+                rl.question('> ', function (saisie) {
+
+                    console.log('...Recherche en cours du nom  : ', saisie);
+                    service.rechercherCollegueParNom(saisie,
+                        function (err, res, body) {
+                            console.log(res.statusCode);
+                            if (res.statusCode === 200) {
+                                console.log(body.length, "collegue(s)  trouvé(s) !");
+
+
+                                for (const key in body) {
+
+                                    console.log(body[key]);
+
+                                }
+                            }
+
+                        }
+                    );
+
+
+                });
                 break;
+
+
+            case "2":
+                console.log("**Création d'un nouveau collegue :**")
+                console.log('Saisissez un nom :');
+
+                rl.question('> ', function (nom) {
+                    console.log('Saisissez un prenom :');
+                    rl.question('> ', function (prenom) {
+                        console.log('Saisissez un email :');
+                        rl.question('> ', function (email) {
+                            console.log('saisissez une date de naissance (annee-mois-jour');
+                            rl.question('> ', function (ddn) {
+                                console.log("saisissez l'url d'une photo");
+                                rl.question('> ', function (photoUrl) {
+
+                                    service.creerCollegue(nom, prenom, email, ddn, photoUrl,
+                                        function (err, res, body) {
+                                            console.log(res.statusCode);
+                                            if (res.statusCode === 200) {
+                                                console.log("collegue  ajouté !");
+                                                console.log(body);
+                                            }
+                                        }
+                                    );
+                                });
+
+                            });
+
+                        });
+
+                    });
+
+                });
+
+
+
+
+
+                break;
+
+
+            case "3":
+                console.log("**Modification email**")
+                console.log("Saisissez le matricule :");
+
+
+                rl.question('> ', function (matricule) {
+
+                    console.log("Saisissez le nouvel email :");
+                    rl.question('> ', function (email) {
+                        service.modifierEmail(email, matricule,
+                            function (err, res, body) {
+                                console.log(res.statusCode);
+                                if (res.statusCode === 200) {
+                                    console.log("email modifié !");
+                                    console.log(body);
+
+
+                                }
+
+                            }
+                        );
+                    });
+
+
+
+                });
+                break;
+
+
+            case "4":
+                console.log("**Modification de la photo**")
+                console.log("Saisissez le matricule :");
+
+                rl.question('> ', function (matricule) {
+
+                    console.log("Saisissez le nouvel email :");
+                    rl.question('> ', function (photo) {
+                        service.modifierPhoto(photo, matricule,
+                            function (err, res, body) {
+                                console.log(res.statusCode);
+                                if (res.statusCode === 200) {
+                                    console.log("photo modifié !");
+                                    console.log(body);
+
+                                }
+
+                            }
+                        );
+                    });
+
+
+                });
+                break;
+
             case "99":
-                console.log('Aurevoir');
+                console.log('bye bye');
                 rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
                 break;
             default:
@@ -87,11 +207,7 @@ function menu() {
                 break;
         }
 
-
-
     });
-
-
 
 
 }
--- service.js
+++ service.js
@@ -3,11 +3,14 @@
 // création d'une requête avec activation de suivi de Cookies.
 var request = require('request').defaults({ jar: true });
 
+const urlApi = 'https://kseguineau-collegues-api.herokuapp.com/';
+
+
 function initPost(loginUser, mdpUser, callbackFn) {
     //console.log("TCL: initPost -> loginUser, mdpUser", loginUser, mdpUser)
 
 
-    request('https://kseguineau-collegues-api.herokuapp.com/auth',
+    request(urlApi + 'auth',
         {
             method: 'POST',
             json: true,
@@ -17,32 +20,137 @@ function initPost(loginUser, mdpUser, callbackFn) {
             }
         },
         function (err, res) {
-            request('https://kseguineau-collegues-api.herokuapp.com/collegues',
-                {
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        login: loginUser,
-                        mdp: mdpUser
-                    }
-                },
-                function (err, res) {
-
-                    //callbackFn(err, res);
-                }
-            );
-
             callbackFn(err, res);
         }
     );
 }
 
-module.exports = {
-    initPost: initPost
+
+
+function rechercherCollegueParNom(nomCollegue, callbackFn) {
+    request(urlApi + 'collegues?nom=' + nomCollegue,
+        {
+            method: 'GET',
+            json: true
+
+        },
+        function (err, res, matricule) {
+            var nbr = matricule.length;
+
+            if (res.statusCode === 200 && matricule[0] != undefined) {
+
+                for (let index = 0; index < nbr; index++) {
+                    request(urlApi + 'collegues/' + matricule[index],
+                        {
+                            method: 'GET',
+                            json: true
+                        },
+                        function (err, res, body) {
+                            callbackFn(err, res, body);
+                        }
+                    );
+
+                }
+
+
+            } else {
+                console.log('nom inconnu')
+            }
+
+
+
+            //callbackFn(err, res, body);
+
+        }
+    );
+}
+
+function creerCollegue(nom, prenom, email, ddn, photoUrl, callbackFn) {
+
+
+
+    request(urlApi + 'collegues',
+        {
+            method: 'POST',
+            json: true,
+            body: {
+                "nom": nom,
+                "prenom": prenom,
+                "email": email,
+                "ddn": ddn,
+                "photoUrl": photoUrl
+            }
+        },
+        function (err, res, body) {
+            callbackFn(err, res, body);
+        }
+    );
+}
+
+
+function modifierEmail(nouvEmail, matricule, callbackFn) {
+
+    request(urlApi + 'collegues/' + matricule,
+        {
+            method: 'PATCH',
+            json: true,
+            body: {
+                "email": nouvEmail
+            }
+
+        },
+        function (err, res, body) {
+            console.log(res.statusCode, res.statusMessage)
+
+            if (res.statusCode === 200) {
+
+                callbackFn(err, res, body);
+
+
+            } else {
+                console.log('error')
+            }
+
+
+        }
+    );
+}
+
+function modifierPhoto(nouvPhoto, matricule, callbackFn) {
+
+    request(urlApi + 'collegues/' + matricule,
+        {
+            method: 'PATCH',
+            json: true,
+            body: {
+                "photoUrl": nouvPhoto
+            }
+
+        },
+        function (err, res, body) {
+            console.log(res.statusCode, res.statusMessage)
+
+            if (res.statusCode === 200) {
+
+                callbackFn(err, res, body);
+
+
+            } else {
+                console.log('error')
+            }
+
+
+        }
+    );
 }
 
 
 
-function recupererCollegues(params) {
+module.exports = {
+    initPost: initPost,
+    rechercherCollegueParNom: rechercherCollegueParNom,
+    creerCollegue: creerCollegue,
+    modifierEmail: modifierEmail,
+    modifierPhoto: modifierPhoto
 
 }
